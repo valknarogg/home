95bbd6bcde909e85b7b401d0a3ea1d65141a42ba
Sebastian Krüger <valknar@pivoine.art>
2025-10-09 14:47:27 +0200

    feat: docs

--- Projects/kompose/docs/nuxt.config.ts
+++ Projects/kompose/docs/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/kompose/',
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
