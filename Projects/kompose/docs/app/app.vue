9da894975239b81b82a2f1e770c44aab8bfdf02b
Sebastian Krüger <valknar@pivoine.art>
2025-10-09 15:25:44 +0200

    feat: docs

--- Projects/kompose/docs/app/app.vue
+++ Projects/kompose/docs/app/app.vue
@@ -11,7 +11,7 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
   ],
   link: [
-    { rel: 'icon', href: '/kompose/favicon.ico' }
+    { rel: 'icon', href: 'favicon.ico' }
   ],
   htmlAttrs: {
     lang: 'en'
