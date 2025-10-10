f0aabd63b6fa4aad0e128df2e3d7df09d9baca88
Sebastian Krüger <valknar@pivoine.art>
2025-10-10 16:31:54 +0200

    fix: gitignore

--- .init/bin/mirror_project.sh
+++ .init/bin/mirror_project.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 TMP_PROJECT="$PWD"
-RELATIVE_HOME="$TMP_PROJECT"
-CURRENT_PROJECT="${s##*/}"
+CURRENT_PROJECT="$TMP_PROJECT"
+RELATIVE_HOME="${CURRENT_PROJECT##*/}"
+ABSOLUTE_HOME="${RELATIVE_HOME}/home"
 
-cp -rf $RELATIVE_HOME/Projects/kompose/* $TMP_PROJECT
-rm -rf $TMP_PROJECT/.env $TMP_PROJECT/**/uploads/ $TMP_PROJECT/**/*.sql $TMP_PROJECT/**/*.pem
+cp -rf $ABSOLUTE_HOME/Projects/kompose/* $CURRENT_PROJECT
+rm -rf $CURRENT_PROJECT/.env $CURRENT_PROJECT/**/uploads/ $CURRENT_PROJECT/**/*.sql $CURRENT_PROJECT/**/*.pem
 
 if [[ `git add -A && git diff --quiet && git diff --cached --quiet` ]]; then
   # Changes
