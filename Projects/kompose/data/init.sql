1b2dc18fd7616ee365035e2cb867428500e8d399
Sebastian Krüger <valknar@pivoine.art>
2025-10-11 18:44:20 +0200

    repo: refactoring

--- Projects/kompose/data/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE directus;
-CREATE DATABASE letterspace;
-CREATE DATABASE umami;
-CREATE DATABASE keycloak;
-
-CREATE USER monitoring WITH PASSWORD 'monitoring';
-GRANT pg_monitor TO monitoring;
-GRANT SELECT ON pg_stat_database TO monitoring;
